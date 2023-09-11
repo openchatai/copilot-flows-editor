404e4b699a77a6c4ee72d7374f532091641f23e6
Ahmad Hassan <76843311+ah7255703@users.noreply.github.com>
2023-09-12 01:01:43 +0300

    removed unsed code

--- src/components/FlowBuilder/MutateNodesProvider.ts
+++
@@ -1,7 +0,0 @@
-import { createSafeContext } from "../../utils/create-safe-context";
-import type { Node } from "reactflow";
-const [MutateNodesProvider, useMutateNodes] = createSafeContext<{ nodes: Node[] }>({
-    nodes: []
-});
-
-export { MutateNodesProvider, useMutateNodes };
--- src/components/FlowBuilder/activeNodeProvider.ts
+++
@@ -1,7 +0,0 @@
-import { createSafeContext } from "../../utils/create-safe-context";
-import type { Node } from "reactflow";
-const [ActiveNodeProvider, useActiveNode] = createSafeContext<{ activeNode: Node | undefined }>(
-    { activeNode: undefined }
-);
-
-export { ActiveNodeProvider, useActiveNode };
