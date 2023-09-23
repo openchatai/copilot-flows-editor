78034c5241bff4a5b9d3702bf3ed9a91156514c9
Ahmad Hassan <76843311+ah7255703@users.noreply.github.com>
2023-09-23 20:40:02 +0300

    vite's extra configurations

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import { sentryVitePlugin } from "@sentry/vite-plugin";
 export default defineConfig(({ mode }) => {
   const { SENTRY_AUTH_TOKEN } = Object.assign(process.env, loadEnv(mode, process.cwd(), ''))
   return {
+    root: __dirname,
+    base: './',
     plugins: [react(), sentryVitePlugin({
       authToken: SENTRY_AUTH_TOKEN,
       org: "openchat-ai-e588264b7",
