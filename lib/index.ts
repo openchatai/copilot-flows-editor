2beec39f48358d4fa64e5a30f812041ff66a8cac
Ahmad Hassan <76843311+ah7255703@users.noreply.github.com>
2023-09-29 20:57:41 +0300

    docs(changeset): export some useful utils

--- lib/index.ts
+++ lib/index.ts
@@ -2,5 +2,10 @@ import "../styles/index.css";
 export { FlowArena } from "./editor/FlowArena";
 export { CodePreview } from "./editor/CodePreview";
 export { Controller, useController } from "./stores/Controller";
-export type { Paths, TransformedPath } from './types/Swagger'
-export type { Flow, FlowSchema, Step } from './types/Flow'
+export type { Paths, TransformedPath } from "./types/Swagger";
+export type { Flow, FlowSchema, Step } from "./types/Flow";
+// some exported utils
+export {
+  transformaEndpointToNode,
+  trasnformEndpointNodesData,
+} from "./utils/transformEndpointNodes";
